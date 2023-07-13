83e45ea23930f1a530f8c7559a2622232034e21f
wughangar <wughangar.rose@gmail.com>
2023-07-13 07:41:17 +0000

    file

--- models/base_model.py
+++ models/base_model.py
@@ -75,16 +75,3 @@ class BaseModel:
                 except TypeError:
                     json_dict[key] = str(value)
         return json_dict
-
-all_objs = storage.all()
-print("-- Reloaded objects --")
-for obj_id in all_objs.keys():
-    obj = all_objs[obj_id]
-    print(obj)
-
-print("-- Create a new object --")
-my_model = BaseModel()
-my_model.name = "My_First_Model"
-my_model.my_number = 89
-my_model.save()
-print(my_model)
