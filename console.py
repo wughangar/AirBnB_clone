271a5a0be8acf728e2c6788fa082059f29f440f8
wughangar <wughangar.rose@gmail.com>
2023-07-13 13:42:40 +0000

    console

--- console.py
+++ console.py
@@ -23,5 +23,44 @@ class HBNBCommand(cmd.Cmd):
         """when no commad is entered nothing happens"""
         pass
 
+    def do_create(self, arg):
+        """
+        create command that creates a new instance of BaseModel, saves to 
+        Json and prints the id
+        """
+        commands = arg.split(' ')
+        class_name = commands[0].strip()
+        if class_name == "":
+            print("** class name missing **")
+        elif class_name not in globals():
+            print("** class doesnt exist **")
+        else:
+            cls = globals().get(class_name)
+            obj = cls()
+            obj.save()
+            print(obj.id)
+
+    def do_show(self, arg):
+        """ prints str rep on an isntance of a class"""
+        commands = arg.split(' ')
+
+        class_name = commands[0].strip()
+        if class_name == "":
+            print("** class name missing **")
+        elif class_name not in globals():
+            print("** class doesn't exist **")
+        id = commands[1].strip()
+
+        if id == "":
+            print("** instance id missing **")
+        elif class_name not in globals():
+            print("** no instance found **")
+        else:
+            cls = globals().get(class_name)
+            obj =cls()
+            print(obj.__str__)
+
+            
+
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
