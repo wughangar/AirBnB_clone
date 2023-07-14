d14bd1497c11e418f68437050fd30fa6616fce93
wughangar <wughangar.rose@gmail.com>
2023-07-14 06:14:34 +0000

    updated file storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -4,6 +4,10 @@ file storage class
 """
 import json
 from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
 
 class FileStorage:
     """
